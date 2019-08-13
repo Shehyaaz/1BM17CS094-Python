6223156487307a9a4572b462665b1cd2e562da06
Shehyaaz <49763208+Shehyaaz@users.noreply.github.com>
2019-08-13 15:29:19 +0530

    completed program 1b

--- skn_py1b.py
+++ skn_py1b.py
@@ -3,9 +3,12 @@ def inputNum():
     return int(input("Enter number of elements (greater than 2):"))
 
 def fib(n):
-    fibList = [0,1]
-    for i in range(2,n):
-        fibList.append(fibList[i-2]+fibList[i-1])
-    print(fibList)
+    if n == 1:
+        print(0)
+    else :
+        fibList = [0,1]
+        for i in range(2,n):
+            fibList.append(fibList[i-2]+fibList[i-1])
+        print(fibList)
             
 fib(inputNum())
